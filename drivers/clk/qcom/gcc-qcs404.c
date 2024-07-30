6d1d64e7d2f1f554826a9203bf5071db3e4705f0
Ajit Pandey <quic_ajipan@quicinc.com>
2024-07-30 17:44:53 +0530

    clk: qcom: gcc-qcs404: Update name for external phy clocks
    Few of hdmi and dsi clocks are registered by separate driver modules
    as these are external to gcc controller. Update clock names of clocks
    for configuring correct parent muxes for these external clocks.
    
    Change-Id: Ib06e9a4a27f6e3c713cf7498e9e20f846e26f93e
    Signed-off-by: Ajit Pandey <quic_ajipan@quicinc.com>

--- drivers/clk/qcom/gcc-qcs404.c
+++ drivers/clk/qcom/gcc-qcs404.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (c) 2018, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/kernel.h>
@@ -282,7 +282,7 @@ static const struct parent_map gcc_parent_map_5[] = {
 
 static const struct clk_parent_data gcc_parent_data_5[] = {
 	{ .fw_name = "bi_tcxo" },
-	{ .fw_name = "dsi0pll_byteclk_src" },
+	{ .fw_name = "dsi0pll_byteclk_src", .name = "dsi0pll_byteclk_src" },
 	{ .hw = &gpll0_out_main.clkr.hw },
 };
 
@@ -294,7 +294,7 @@ static const struct parent_map gcc_parent_map_6[] = {
 
 static const struct clk_parent_data gcc_parent_data_6[] = {
 	{ .fw_name = "bi_tcxo" },
-	{ .fw_name = "dsi0_phy_pll_out_byteclk" },
+	{ .fw_name = "dsi0_phy_pll_out_byteclk", .name = "dsi0pll_byteclk_src" },
 	{ .hw = &gpll0_out_main.clkr.hw },
 };
 
@@ -321,7 +321,7 @@ static const struct parent_map gcc_parent_map_8[] = {
 
 static const struct clk_parent_data gcc_parent_data_8[] = {
 	{ .fw_name = "bi_tcxo" },
-	{ .fw_name = "hdmi_phy_pll_clk" },
+	{ .fw_name = "hdmi_phy_pll_clk", .name = "hdmi_phy_pll_clk" },
 };
 
 static const struct parent_map gcc_parent_map_9[] = {
@@ -334,7 +334,7 @@ static const struct parent_map gcc_parent_map_9[] = {
 static const struct clk_parent_data gcc_parent_data_9[] = {
 	{ .fw_name = "bi_tcxo" },
 	{ .hw = &gpll0_out_main.clkr.hw },
-	{ .fw_name = "dsi0_phy_pll_out_dsiclk" },
+	{ .fw_name = "dsi0_phy_pll_out_dsiclk", .name = "dsi0pll_pclk_src" },
 	{ .hw = &gpll6.clkr.hw },
 
 };
@@ -367,7 +367,7 @@ static const struct parent_map gcc_parent_map_12[] = {
 
 static const struct clk_parent_data gcc_parent_data_12[] = {
 	{ .fw_name = "bi_tcxo" },
-	{ .fw_name = "dsi0pll_pclk_src" },
+	{ .fw_name = "dsi0pll_pclk_src", .name = "dsi0pll_pclk_src" },
 	{ .hw = &gpll0_out_main.clkr.hw },
 };
 
