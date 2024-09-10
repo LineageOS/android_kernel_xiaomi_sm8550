5621c58c04045fbdb94624e44f23380d6b6b94b1
Shilpa Suresh <quic_c_sbsure@quicinc.com>
2024-09-10 12:20:29 +0530

    leds: qpnp-flash: Revert the common flash API support
    Remove the "qpnp_flash_register_led_prepare" design introduced
    via commit 79638222d895 ("leds: qpnp-flash: Introduce a
    "qpnp_flash_register_led_prepare""), since it was causing a
    compilation error when the camera clients were using the legacy
    interface APIs. The common flash API support will be brought back
    once all the impacted camera components are updated with the
    common API usage.
    
    Change-Id: Icad0ebd9f7170a95562036fe4435889d0dbea23b
    Signed-off-by: Shilpa Suresh <quic_c_sbsure@quicinc.com>
    (cherry picked from commit cc9e554ecc0a05be4fe4c73200d0d5012ef5a534)

--- include/linux/leds-qti-flash.h
+++ include/linux/leds-qti-flash.h
@@ -23,24 +23,22 @@ struct flash_led_param {
 	u64 off_time_ms;
 };
 
-int qpnp_flash_register_led_prepare(struct device *dev, void *data);
-
-#if (IS_ENABLED(CONFIG_LEDS_QTI_FLASH) || IS_ENABLED(CONFIG_LEDS_QPNP_FLASH_V2))
-int qpnp_flash_led_prepare(struct led_trigger *trig, int options,
-					int *max_current);
+#if IS_ENABLED(CONFIG_LEDS_QTI_FLASH)
+int qti_flash_led_prepare(struct led_trigger *trig,
+			int options, int *max_current);
 int qti_flash_led_set_param(struct led_trigger *trig,
 			struct flash_led_param param);
 #else
-static inline int qpnp_flash_led_prepare(struct led_trigger *trig, int options,
-					int *max_current)
+static inline int qti_flash_led_prepare(struct led_trigger *trig,
+					int options, int *max_current)
 {
-	return -ENODEV;
+	return -EINVAL;
 }
 
 static inline int qti_flash_led_set_param(struct led_trigger *trig,
 			struct flash_led_param param)
 {
-	return -ENODEV;
+	return -EINVAL;
 }
 #endif
 #endif
